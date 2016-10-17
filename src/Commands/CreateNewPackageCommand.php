80c93f37e85852a184fef6f68d8dcf794c623ed7
Mace Muilman <mace@macemuilman.nl>
2016-10-17 21:24:47 +0200

    Overhauled summary display.

--- src/Commands/CreateNewPackageCommand.php
+++ src/Commands/CreateNewPackageCommand.php
@@ -30,9 +30,28 @@ class CreateNewPackageCommand extends Command {
         $author = $this->ask('What is your name?');
         $email = $this->ask('What is your email?');
 
-        $this->info('Summary: Your name is ' . $author . ', your email adress is ' . $email . '. The package you want to create is called ' . $vendor . '/' . $package . '.');
+        $makeRepository = $this->confirm('Do you wish to start a git repository for this package?');
+        if ($makeRepository){
+            $existingRepository = $this->confirm('Do you already have a remote repository for this package?');
+            if ($existingRepository) {
+                $remoteRepository = $this->ask('What is the name of the remote repository? (Example: mace015/package-creator)');
+            }
+        }
 
-        if ($this->confirm('Is this correct? [yes|no]')) {
+        $this->comment('Please check if the following details are correct;');
+
+        $this->table(['Question', 'Answer'],[
+            ['Your vendor name is: ', $vendor],
+            ['Your package name is: ', $package],
+            ['The description of your package is: ', $description],
+            ['The namespace of your package is: ', $namespace],
+            ['Your name is: ', $author],
+            ['Your email is: ', $email],
+            ['Start a git repository?', (($makeRepository)? 'Yes' : 'No')],
+            ['Connect a remote repository?', (($existingRepository)? 'Yes: ' . $remoteRepository : 'No')],
+        ], 'default');
+
+        if ($this->confirm('Is this correct?')) {
 
             $path = base_path() . '/packages/' . ucfirst($vendor) . '/' . ucfirst($package);
 
@@ -40,17 +59,15 @@ class CreateNewPackageCommand extends Command {
             @$this->file->put($path . '/composer.json', $this->getComposer($vendor, $package, $description, $namespace, $author, $email));
             @$this->file->put($path . '/src/' . $namespace . 'ServiceProvider.php', $this->getServiceProvider($vendor, $namespace));
 
-            if($this->confirm('Do you wish to start a git repository for this package? [yes|no]')){
+            if($makeRepository){
 
                 exec('cd packages/'. ucfirst($vendor) .'/'. ucfirst($package) .' && git init');
 
                 $this->info('Git repository initialised.');
 
-                if ($this->confirm('Do you already have a remote repository for this package? [yes|no]')) {
+                if ($existingRepository) {
 
-                    $git = $this->ask('What is the name of the remote repository? (Example: mace015/package-creator)');
-
-                    exec('cd packages/'. ucfirst($vendor) .'/'. ucfirst($package) .' && git remote add origin git@github.com:'. $git .'.git');
+                    exec('cd packages/'. ucfirst($vendor) .'/'. ucfirst($package) .' && git remote add origin git@github.com:'. $remoteRepository .'.git');
 
                     $this->info('Remote repository added.');
 
